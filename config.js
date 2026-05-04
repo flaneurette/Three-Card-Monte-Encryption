71dd80975c90069a8a2bf8833a51ed5e68c52417
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2026-05-04 16:20:01 +0200

    Add initial configuration settings for security

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // Security settings
+  password: "correct-horse-battery-staple", // Replace with a strong Diceware password
+  hardwareKeySize: 128,                     // Bytes to slice and store as hardware key (e.g., USB drive)
+  argon2: {
+    timeCost: 6,                            // ~3s delay
+    memoryCost: 524288,                     // 512MB RAM
+    parallelism: 1,                         // number of required cores. Increase if required.
+  }, 
+  totalFragments: 10,                       // 10 puzzle fragments already makes it impracticle to bruteforce. Can be increased, but requires more storage.
+  dummyCount: 6,                            // 6 dummy files (total 10 puzzle fragments)
+  fragmentCount: 4,                         // Split real file into 4 puzzle pieces
+  pinPositions: [4,3,2,1],                  // Positions of real puzzle fragments in the archive (this is your unique PINCODE!)
+  minFragmentSize: 200,                     // Minimum amount of characters of dummy fragments.
+  maxFragmentSize: 10000,                   // Maximum amount of characters of dummy fragments.
+  // File paths
+  inputFile: "./secret.txt",                // File to encrypt
+  encryptedArchive: 'enecypted-file.tcm',
+  decryptedFile: 'decrypted-secret.txt',
+  hardwareKeyPath: 'hardware-key.bin',
+  fragmentSize: 4096,  // Must match encryption
+};
